6011d9cfa2a4381033da43b5196abdd9460c1bc6
Keeganw3 <keeganw749@gmail.com>
2022-07-22 19:47:11 +0000

    [ADD] Navbar text added

--- pianos_r_us/settings.py
+++ pianos_r_us/settings.py
@@ -13,8 +13,7 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -36,7 +35,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
     'django.contrib.sites',
 
     'allauth',
