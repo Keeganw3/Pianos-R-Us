b20c0433c3d19e2f8edab6bae088d72d382c0da1
Keeganw3 <keeganw749@gmail.com>
2022-08-13 00:58:06 +0000

    [ADD] added stripe payment and card elements to the checkout page

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'checkout_form': checkout_form,
+        'stripe_public_key': 'pk_test_51LK40UI31Ly6KbPUKCD8jZOxuCk5Eqjr84wqz8ZFIMWVl61Chv7Mo8kDXjy3cCSO7nKlWs1btlOmnHiiE4ecFkT200mTgPy9RO',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
