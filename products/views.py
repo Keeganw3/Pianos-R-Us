14813419a1e5f809dd1c86105de54c582d17a228
Keeganw3 <keeganw749@gmail.com>
2022-08-16 22:58:27 +0000

    [ADD] added product delete functionality and url

--- products/views.py
+++ products/views.py
@@ -76,11 +76,12 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Successfully added product!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_view', args=[product.id]))
         else:
-            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
+            messages.error(request, 'Failed to add product. \
+            Please ensure the form is valid.')
     else:
         form = ProductForm()    
     
@@ -89,8 +90,7 @@ def add_product(request):
         'form': form,
     }
 
-    return render(request, template, context
-    )
+    return render(request, template, context)
 
 
 def edit_product(request, product_id):
@@ -103,7 +103,8 @@ def edit_product(request, product_id):
             messages.success(request, 'Successfully updated product!')
             return redirect(reverse('product_view', args=[product.id]))
         else:
-            messages.error(request, 'Failed to update product. Please ensure the form is valid.')
+            messages.error(request, 'Failed to update product. \
+                Please ensure the form is valid.')
     else:
         form = ProductForm(instance=product)
         messages.info(request, f'You are editing {product.name}')
@@ -115,3 +116,11 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    """ Delete a product from the store """
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product deleted!')
+    return redirect(reverse('products'))
