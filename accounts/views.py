e340ea53362f09d02b9af29d7f135f590ffe758a
Keeganw3 <keeganw749@gmail.com>
2022-08-16 02:22:22 +0000

    [ADD] added account views and form and updated template

--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,27 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 
 from .models import UserAccount
+from .forms import UserAccountForm
 
 def account(request):
     """ Displays the user's account. """
     account = get_object_or_404(UserAccount, user=request.user)
 
+    if request.method == 'POST':
+        form = UserAccountForm(request.POST, instance=account)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Account updated successfully')
+
+    form = UserAccountForm(instance=account)
+    orders = account.orders.all()
+
     template = 'accounts/account.html'
     context = {
-        'account': account,
+        'form': form,
+        'orders': orders,
+        'on_account_page': True
     }
 
     return render(request, template, context)
