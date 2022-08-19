6321b9f458d2ec5c3f77c9dea3faf910bc259926
Keeganw3 <keeganw749@gmail.com>
2022-08-19 15:58:02 +0000

    [ADD] addcreated custom page

--- pianos_r_us/urls.py
+++ pianos_r_us/urls.py
@@ -1,22 +1,8 @@
-"""pianos_r_us URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import handler404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,3 +13,5 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('account/', include('accounts.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'pianos_r_us.views.handler404'
